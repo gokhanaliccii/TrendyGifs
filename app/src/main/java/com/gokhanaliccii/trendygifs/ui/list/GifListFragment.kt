85b7a1c375eaf360637c5c48e8e089773f8564c6
gokhanaliccii <gokhanaliccii@gmail.com>
2019-02-24 23:40:02 +0300

    Add load more functionality on GifListFragment

--- app/src/main/java/com/gokhanaliccii/trendygifs/ui/list/GifListFragment.kt
+++ app/src/main/java/com/gokhanaliccii/trendygifs/ui/list/GifListFragment.kt
@@ -2,18 +2,16 @@ package com.gokhanaliccii.trendygifs.ui.list
 
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProvider
-import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.support.v7.widget.RecyclerView
 import com.gokhanaliccii.trendygifs.R
 import com.gokhanaliccii.trendygifs.core.base.BindableFragment
 import com.gokhanaliccii.trendygifs.core.viewmodel.ViewModelFactory
 import com.gokhanaliccii.trendygifs.databinding.FragmentGifListBinding
 import com.gokhanaliccii.trendygifs.ui.list.adapter.GifListAdapter
 import com.gokhanaliccii.trendygifs.util.gifloader.GlideGifLoader
+import com.gokhanaliccii.trendygifs.util.recyclerview.RecyclerViewLoadMoreListener
 
 /**
  * Created by gokhan.alici on 24.02.2019
@@ -26,16 +24,24 @@ class GifListFragment : BindableFragment<FragmentGifListBinding, GifListViewMode
     override val viewModelFactory: ViewModelProvider.Factory?
         get() = ViewModelFactory()
 
+    private lateinit var loadMoreListener: RecyclerViewLoadMoreListener
+
     override fun getViewModel(): Class<GifListViewModel> = GifListViewModel::class.java
 
     override fun onViewBindingReady(savedInstanceState: Bundle?) {
         val gridLayoutManager = GridLayoutManager(context, getColumnCount())
         val gifListAdapter = GifListAdapter(GlideGifLoader())
 
+        loadMoreListener = RecyclerViewLoadMoreListener(gridLayoutManager) {
+            viewModel.loadGifs()
+        }
+
         viewBinding.recyclerViewGif.run {
             layoutManager = gridLayoutManager
             adapter = gifListAdapter
         }
+
+        viewBinding.recyclerViewGif.addOnScrollListener(loadMoreListener)
 
         viewModel.gifList.observe(this,
             Observer {
@@ -51,5 +57,12 @@ class GifListFragment : BindableFragment<FragmentGifListBinding, GifListViewMode
 
     private fun getColumnCount(): Int {
         return resources.getInteger(R.integer.trend_list_column_count)
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        viewBinding.recyclerViewGif.removeOnScrollListener(loadMoreListener)
     }
 }
