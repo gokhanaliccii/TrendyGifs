8fac249dc5b8bae9b5821ae822c5b60542c3bd50
gokhanaliccii <gokhanaliccii@gmail.com>
2019-02-24 18:57:44 +0300

    Add giphy api base url on properties file

--- data/src/main/java/com/gokhanaliccii/trendygifs/data/api/GiphyApiFactory.kt
+++ data/src/main/java/com/gokhanaliccii/trendygifs/data/api/GiphyApiFactory.kt
@@ -4,4 +4,12 @@ package com.gokhanaliccii.trendygifs.data.api
  * Created by gokhan.alici on 24.02.2019
  */
 class ApiFacade {
+
+    private companion object {
+        const val CONNECTION_TIMEOUT = 3
+    }
+
+    fun createGiphyApi(){
+
+    }
 }
