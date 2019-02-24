668a68c1482835f2be1bbfba1a65f7d83649be54
gokhanaliccii <gokhanaliccii@gmail.com>
2019-02-24 19:04:11 +0300

    Add GiphyApiFactory which create giphy api

--- data/src/main/java/com/gokhanaliccii/trendygifs/data/api/GiphyApiFactory.kt
+++ data/src/main/java/com/gokhanaliccii/trendygifs/data/api/GiphyApiFactory.kt
@@ -1,15 +1,57 @@
 package com.gokhanaliccii.trendygifs.data.api
 
+import com.gokhanaliccii.trendygifs.data.BuildConfig
+import com.gokhanaliccii.trendygifs.data.api.interceptor.AuthKeyInterceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
 /**
  * Created by gokhan.alici on 24.02.2019
  */
-class ApiFacade {
+internal object GiphyApiFactory {
 
-    private companion object {
-        const val CONNECTION_TIMEOUT = 3
+    private val retrofit by lazy {
+        ApiFacade().createRetrofit()
     }
 
-    fun createGiphyApi(){
+    fun createGiphyApi() = retrofit.create(GiphyApi::class.java)
 
+    private class ApiFacade {
+        private companion object {
+            const val CONNECTION_TIMEOUT = 3L
+            const val READ_TIMEOUT = 3L
+        }
+
+        fun createRetrofit(): Retrofit {
+            buildHttpClient()
+
+            return Retrofit.Builder()
+                .baseUrl(BuildConfig.GIPHY_API_URL)
+                .client(buildHttpClient())
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
+        }
+
+        private fun buildHttpClient(): OkHttpClient {
+            val httpClientBuilder = OkHttpClient.Builder()
+                .connectTimeout(CONNECTION_TIMEOUT, TimeUnit.SECONDS)
+                .connectTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
+                .addInterceptor(AuthKeyInterceptor())
+
+            if (BuildConfig.DEBUG) {
+                httpClientBuilder.addInterceptor(
+                    HttpLoggingInterceptor().setLevel(
+                        HttpLoggingInterceptor.Level.BODY
+                    )
+                )
+            }
+
+            return httpClientBuilder.build()
+        }
     }
 }
